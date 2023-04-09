92c41357951ecd5fc19874b531ed365079a36eab
Damir <delewv.damir@gmail.com>
2023-04-09 17:32:02 +0300

    vuew

---
+++ resources/js/components/Person/Show.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-if="person">
+        <div>
+            Name: {{this.person.name}}
+        </div>
+        <div>
+            age: {{this.person.age}}
+        </div>
+        <div>
+            job: {{this.person.job}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Show",
+
+    data(){
+        return{
+            person: null
+        }
+    },
+    mounted() {
+        this.getPerson()
+    },
+
+    methods: {
+        getPerson(id){
+            axios.get('/api/people/' + this.$route.params.id)
+                .then(res => {
+                    this.person = res.data
+                })
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
